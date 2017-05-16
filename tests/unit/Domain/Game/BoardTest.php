ec95c5b21c169724adb27b2cbecfc7e04515d3f5
Markus Reinhold <markusreinhold@icloud.com>
2017-05-16 18:42:35 +0200

    Add tests for Board::dropStone

--- tests/unit/Domain/Game/BoardTest.php
+++ tests/unit/Domain/Game/BoardTest.php
@@ -2,43 +2,58 @@
 
 namespace Marein\ConnectFour\Domain\Game;
 
+use Marein\ConnectFour\Domain\Game\Exception\ColumnAlreadyFilledException;
+use Marein\ConnectFour\Domain\Game\WinningStrategy\CommonWinningStrategy;
 use PHPUnit\Framework\TestCase;
 
 class BoardTest extends TestCase
 {
     /**
-     * All the tests of the class game should cover these tests. So check for immutability.
-     *
-     * @todo Move tests for [Board] from GameTest.php to BoardTest.php
+     * @test
      */
+    public function aStoneCanBeDropped(): void
+    {
+        $configuration = Configuration::custom(
+            new Size(7, 6),
+            new CommonWinningStrategy()
+        );
+        $board = Board::empty($configuration);
+        $boardCopy = clone $board;
+
+        $boardWithStone = $board->dropStone(Stone::red(), 1);
+        $expectedPoint = new Point(1, 6);
+        $fields = $boardWithStone->findFieldsByPoints([$expectedPoint]);
+        $affectedField = array_pop($fields);
+        $this->assertEquals(Stone::red(), $affectedField->stone());
+
+        $boardWithStone = $boardWithStone->dropStone(Stone::yellow(), 1);
+        $expectedPoint = new Point(1, 5);
+        $fields = $boardWithStone->findFieldsByPoints([$expectedPoint]);
+        $affectedField = array_pop($fields);
+        $this->assertEquals(Stone::yellow(), $affectedField->stone());
+
+        $this->assertEquals($board, $boardCopy);
+    }
 
     /**
      * @test
      */
-    public function itShouldBeImmutable(): void
+    public function aStoneCanNotBeDroppedWhenColumnAlreadyFilled(): void
     {
-        $configuration = Configuration::common();
+        $this->expectException(ColumnAlreadyFilledException::class);
+
+        $configuration = Configuration::custom(
+            new Size(7, 6),
+            new CommonWinningStrategy()
+        );
         $board = Board::empty($configuration);
 
-        $hashOfOriginalBoardBeforeChanges = $this->createBoardHash($board);
-
-        $changedBoard = $board->dropStone(Stone::red(), 1);
-
-        $hashOfOriginalBoardAfterChanges = $this->createBoardHash($board);
-
-        $hashOfChangedBoard = $this->createBoardHash($changedBoard);
-
-        $this->assertEquals($hashOfOriginalBoardBeforeChanges, $hashOfOriginalBoardAfterChanges);
-        $this->assertNotEquals($hashOfOriginalBoardBeforeChanges, $hashOfChangedBoard);
-    }
-
-    /**
-     * @param Board $board
-     *
-     * @return string
-     */
-    private function createBoardHash(Board $board): string
-    {
-        return md5(serialize($board));
+        $boardWithStone = $board->dropStone(Stone::red(), 1);
+        $boardWithStone = $boardWithStone->dropStone(Stone::yellow(), 1);
+        $boardWithStone = $boardWithStone->dropStone(Stone::red(), 1);
+        $boardWithStone = $boardWithStone->dropStone(Stone::yellow(), 1);
+        $boardWithStone = $boardWithStone->dropStone(Stone::red(), 1);
+        $boardWithStone = $boardWithStone->dropStone(Stone::red(), 1);
+        $boardWithStone->dropStone(Stone::yellow(), 1);
     }
 }
